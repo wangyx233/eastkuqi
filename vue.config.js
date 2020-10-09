4dea643f2675ca84978e8be78a13f37ea8fa4c05
wangyx233 <1505963263@qq.com>
2020-10-09 17:09:07 +0800

    add publicPath

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Since: 2020-10-09 17:04:52
+ * @LastAuthor: Yixuan
+ * @LastTime: 2020-10-09 17:07:42
+ */
+module.exports = {
+  // 部署应用包时的基本 URL
+  publicPath: process.env.NODE_ENV === 'production' ? '/eastkuqi/' : '/',
+};
